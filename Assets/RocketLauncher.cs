7dea5bb14433f0b4f34f44e8c7c6ca2db515b6b8
unknown <carlosdanielrod@gmail.com>
2023-03-30 21:53:21 -0500

    first parabollic missile launch

--- Assets/RocketLauncher.cs
+++ Assets/RocketLauncher.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class RocketLauncher : MonoBehaviour
 {
     public Transform spawnPoint;
-    
+
     public GameObject MissilePrefab;
 
-    public void Start()
-    {
-        var newMissile = Instantiate(MissilePrefab, spawnPoint.position, spawnPoint.rotation);
+    private GameObject newMissile;
 
-        newMissile.transform.parent = gameObject.transform;
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            newMissile = Instantiate(MissilePrefab, spawnPoint.position, spawnPoint.rotation);
+
+            newMissile.transform.parent = gameObject.transform;
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(newMissile != null)
+            {
+                var missileRb = newMissile.AddComponent<Rigidbody>();
+
+                missileRb.AddForce(new Vector3(spawnPoint.rotation.x, spawnPoint.rotation.y) * 1000f, ForceMode.Force);
+                Destroy(newMissile, 3);
+            }
+        }
     }
 }
